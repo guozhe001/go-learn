8aa5c74d50d7b9114c316f442ee916de4ccfc6c1
guozhe <guozhe513@hotmail.com>
2020-12-22 00:08:49 +0800

    headfirstgo：第四章华氏转摄氏代码练习

---
+++ headfirstgo/ch4/tocelsius.go
@@ -0,0 +1,36 @@
+// 允许用户输入一个华氏测量系统的温度，并将其转换为摄氏系统的温度
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	fmt.Print("Enter a temperature in Fahrenheit:")
+	fahrenheit, err := getFloat()
+	if err != nil {
+		log.Fatal(err)
+	}
+	celsius := (fahrenheit - 32) * 5 / 9
+	fmt.Printf("%0.2f degrees Celsius\n", celsius)
+
+}
+
+func getFloat() (float64, error) {
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		return 0, err
+	}
+	input = strings.TrimSpace(input)
+	number, err := strconv.ParseFloat(input, 64)
+	if err != nil {
+		return 0, err
+	}
+	return number, nil
+}
