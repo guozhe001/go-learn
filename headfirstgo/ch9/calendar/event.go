2769ecb5600db63d9d8a46f087d7da1ec8820a67
guozhe <guozhe513@hotmail.com>
2021-01-03 21:37:09 +0800

    headfirstgo:添加嵌入练习

---
+++ headfirstgo/ch9/calendar/event.go
@@ -0,0 +1,28 @@
+package calendar
+
+import (
+	"errors"
+	"unicode/utf8"
+
+	"github.com/guozhe001/go-learn/headfirstgo/ch9/calendar"
+)
+
+// Event 事件
+type Event struct {
+	title string
+	calendar.Date
+}
+
+// SetTitle 设置标题
+func (e *Event) SetTitle(title string) error {
+	if utf8.RuneCountInString(title) > 30 {
+		return errors.New("invalid title")
+	}
+	e.title = title
+	return nil
+}
+
+// Title 获取title
+func (e *Event) Title() string {
+	return e.title
+}
