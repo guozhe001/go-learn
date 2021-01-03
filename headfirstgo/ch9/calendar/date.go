5d124e7ac6276e4c6ffb531dc06fd66f789e0951
guozhe <guozhe513@hotmail.com>
2021-01-03 20:36:42 +0800

    headfirstgo:add date

---
+++ headfirstgo/ch9/calendar/date.go
@@ -0,0 +1,39 @@
+package calendar
+
+import (
+	"errors"
+)
+
+// Date 日期
+type Date struct {
+	year  int
+	month int
+	day   int
+}
+
+// SetYear 设置年
+func (d *Date) SetYear(year int) error {
+	if year < 1 {
+		return errors.New("invalid year")
+	}
+	d.year = year
+	return nil
+}
+
+// SetMonth 设置月份
+func (d *Date) SetMonth(month int) error {
+	if month < 1 || month > 12 {
+		return errors.New("invalid month")
+	}
+	d.month = month
+	return nil
+}
+
+// SetDay 设置日期
+func (d *Date) SetDay(day int) error {
+	if day < 1 || day > 31 {
+		return errors.New("invalid day")
+	}
+	d.day = day
+	return nil
+}
