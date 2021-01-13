91d7dfab7a25ae240b6e7d3e55a899b06c740acf
guozhe <guozhe@renmaitech.com>
2021-01-13 19:52:11 +0800

    添加练习包

---
+++ practice/stringspractice.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+
+func main(){
+	PrintLastIndex("abc:dae:hi", ":")
+	PrintLastIndex("abc:dae:h:i", ":")
+	PrintLastIndex("abc:daehi", ":")
+}
+
+func PrintLastIndex(s, substr string) {
+	// substr最后一次出现在s中的位置的下标
+	n := strings.LastIndex(s, substr)
+	fmt.Printf("s=%s substr=%s n=%d\n", s, substr, n)
+}
